af7bc3df1b3c75fd976ffce867d55eb4ceabac52
Moises <moyayala12@gmail.com>
2018-06-09 20:09:00 -0700

    Typescript-Recap-Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,33 @@
 import { Component } from '@angular/core';
 
+class CarBrand {
+  make: string;
+
+  constructor(make: string) {
+    this.make = make;
+  }
+}
+
+class Car extends CarBrand {
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    super(make);
+    this.model = model;
+    this.year = year;
+  }
+
+  getDetails() {
+    return ( `The year of the vehicle is ${this.make},
+      the model of the vehicle is ${this.model},
+      of the year ${this.year}
+      `);
+  }
+}
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +35,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ford = new CarBrand('Ford');
+  honda = new Car('Honda', 'Civic', 2018);
 }
