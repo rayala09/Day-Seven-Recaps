2259379e4ef003b581ac64d5c82fe5f0f9ab4b8f
Moises <moyayala12@gmail.com>
2018-06-09 19:46:28 -0700

    Typescript-Recaps-Types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+
+
+const str: string;
+
+const numArr[]: any[] = ['Hello', 1, 3, true];
+
+const num: number;
+
+const bln: boolean;
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
