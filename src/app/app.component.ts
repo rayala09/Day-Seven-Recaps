abaee2d6b75df56352fdcd4d6d7cd75f77b1eaf2
Moises <moyayala12@gmail.com>
2018-06-11 14:16:43 -0700

    Typescript-Recap-Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,23 @@
 import { Component } from '@angular/core';
 
+
+module Vehicle {
+  class Sedan {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+
+    }
+  }
+  const honda = new Sedan('Honda', 'Civic', 2019);
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
