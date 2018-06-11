07752f59d2585794e8130ad2c4ed9b1ad3a60341
Moises <moyayala12@gmail.com>
2018-06-11 01:25:58 -0700

    Typescript-Recap-Interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Fighter {
+  name: string;
+  number: number;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  fighter1: Fighter = {name: 'Ray', number: 14};
+  fighter2: Fighter = {name: 'Raynold', number: 24};
+
+
 }
