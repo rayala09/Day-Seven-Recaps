89ec6e598eaaf2bf0e114cbe7c333c13251682d4
Moises <moyayala12@gmail.com>
2018-06-12 16:01:39 -0700

    Built-In-Directives-Recap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app';
+
+  displayParagraph: boolean = true;
+
+  users = [{
+    name: 'Ray',
+  age: 20}, {name: 'Michael', age: 25}, {name: 'Ronald', age: 23}]
+
+
 }
